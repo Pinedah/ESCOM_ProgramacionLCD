3af68ac974776c7b4d70e8aac726f2da63f0a919
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-10-16 21:00:14 -0600

    ejercicio3 terminado

--- Parcial1/ejercicio03.py
+++ Parcial1/ejercicio03.py
@@ -1,3 +1,27 @@
 # Crear un programa que genere 100 numeros aleatorios entre 1 y 1000
-# inserte en un alista los pares y en otr alos impares
+# inserte en una lista los pares y en otra los impares
 # Imprimir ambas listas y el tamaño de las mismas
+
+# Importar random para generar aleatorios
+import random 
+
+def main():
+
+    # Creación de la lista de aleatorios con List Comprehension
+    aleatorios = [random.randint(1, 1001) for _ in range(100)]
+    
+    # Funciones lambda para filtrar
+    esPar = lambda x: x % 2 == 0
+    esImpar = lambda x: x % 2 == 1
+
+    # Creación de las listas pares e impares con Filter
+    pares = list(filter(esPar, aleatorios))
+    impares = list(filter(esImpar, aleatorios))
+
+    # Impresiones de las listas
+    print(f"\nLista original: \n{aleatorios}\n")
+    print(f"Pares: \n{pares}\n")
+    print(f"Imares: \n{impares}")
+
+if __name__ == "__main__":
+    main()
