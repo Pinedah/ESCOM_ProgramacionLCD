7bd58cf527cb65553131850cf1844cdd0e5db229
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-10-16 20:38:59 -0600

    ejercicio1 terminado

--- Parcial1/ejercicio01.py
+++ Parcial1/ejercicio01.py
@@ -10,9 +10,18 @@ def verificar(cadena):
     # Devuelve: True en caso de que todos los caracteres sean letras ascii
     #           False en caso contrario
 
+    # Reemplazar los espacios con un caracter vacio
+    cadena = cadena.replace(' ', '')
+    for caracter in cadena: 
+        # Iterar sobre cada caracter y verificar si está en la lista que contiene a los ASCII
+        if caracter not in caracteres_ascii:
+            # Devolver False si es que alguno ya no es ASCII
+            return False
+    # Devolver True si es que el se termino el ciclo
+    return True 
     
-    pass
-
-
 cadena1 = "Esto es un a cadena$"
-print(verificar(cadena1))
+print(f"{cadena1}\t-> {verificar(cadena1)}")
+
+cadena2 = "Esto es un a cadena"
+print(f"{cadena2}\t-> {verificar(cadena2)}")
